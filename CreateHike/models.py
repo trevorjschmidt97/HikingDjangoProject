f61635526634d41bfdc421645e00b09c82ec7efa
Trevor Schmidt <trevorjschmidt97@gmail.com>
2020-11-20 10:45:24 -0700

    started models

--- CreateHike/models.py
+++ CreateHike/models.py
@@ -2,11 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Hike(models.Model):
-    userID = models.ManyToOneRel(User)
     name = models.CharField(max_length=30)
-    duration = models.IntegerField
+    duration = models.IntegerField()
     description = models.CharField(max_length=30, blank=True)
-    length = models.FloatField
+    length = models.FloatField()
     location = models.CharField(max_length=30)
 
-    
+    def __str__(self) -> str:
+        return ("Hike: " + self.name)
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    birthday = models.DateField()
+
+    @property
+    def full_name(self):
+        return (self.firstname + " " + self.lastname)
+
+    def __str__(self) -> str:
+        return ("User: " + self.username + "\n\tFirstname: " + self.firstname + "\n\tLastname: " + self.lastname)

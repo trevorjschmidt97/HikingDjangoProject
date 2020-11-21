a2f5c38d6d2b45cdbadd385e6a4ca0b80f09fdb4
miltonmoon <71720075+miltonmoon@users.noreply.github.com>
2020-11-20 17:56:17 -0700

    Update models.py

--- CreateHike/models.py
+++ CreateHike/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-#class Hike(models.Model):
-#    userID = models.ManyToOneRel(User)
-#    name = models.CharField(max_length=30)
-#    duration = models.IntegerField
-#    description = models.CharField(max_length=30, blank=True)
-#    length = models.FloatField
-#    location = models.CharField(max_length=30)
-
 class Hike(models.Model):
     name = models.CharField(max_length=30)
     duration = models.IntegerField()
